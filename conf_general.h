cecc30db42c72d558daabd42fbdc8499c5c143d5
spencer_chen <spencer.chen7@gmail.com>
2021-10-02 16:32:31 +0800

    atr_v4_SPEsc

--- conf_general.h
+++ conf_general.h
@@ -74,8 +74,8 @@
 //#define HW60_IS_MK4
 #define HW60_IS_MK5
 
-#define HW_SOURCE "hw_60.c"
-#define HW_HEADER "hw_60.h"
+//#define HW_SOURCE "hw_60.c"
+//#define HW_HEADER "hw_60.h"
 
 //#define HW_SOURCE "hw_r2.c"
 //#define HW_HEADER "hw_r2.h"
@@ -183,6 +183,9 @@
 
 //#define HW_SOURCE "hw_raiden7.c"
 //#define HW_HEADER "hw_raiden7.h"
+
+#define HW_SOURCE "hw_spesc.c"
+#define HW_HEADER "hw_spesc.h"
 #endif
 
 #ifndef HW_SOURCE
